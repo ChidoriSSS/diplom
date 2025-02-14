a60057349fafe22b2a6cd3436a3db8a52778754d
Leonid <leonidnurdygin3@gmail.com>
2025-02-14 14:09:03 +0400

    14:08

--- oprosnick/surveys/tests.py
+++ oprosnick/surveys/tests.py
@@ -1,5 +1,6 @@
 from surveys.models import Survey
 from surveys.models import User
+from surveys.models import Question
 
 user = User.objects.get(id=1)
 survey = Survey.objects.create(
@@ -7,4 +8,12 @@ survey = Survey.objects.create(
     description = 'dsa',
     created_at = '' ,
     created_by =  user,
+)
+
+sur = Survey.objects.get(id=1)
+print(sur.id)
+question = Question.objects.create(
+    question_text = 'Кнопка',
+    question_type = 'Красный',
+    survey_id = sur.id
 )
